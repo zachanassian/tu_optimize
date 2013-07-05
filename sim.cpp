fef1d52cb126cd3660922a7db7d1119d852fdbbe
9andor@gmail.com <9andor@gmail.com>
2013-07-05 12:14:30 +0800

    Simulate bizarre behavior: On death uses relative-position targeting.

--- sim.cpp
+++ sim.cpp
@@ -1603,7 +1603,7 @@ inline bool skill_predicate<chaos>(Field* fd, CardStatus* src, CardStatus* c, co
     const auto& mod = std::get<4>(s);
     return(!c->m_chaosed && can_act(c) &&  // (fd->tapi == c->m_player ? is_active(c) && !is_attacking_or_has_attacked(c) : is_active_next_turn(c)));
             (mod == SkillMod::on_attacked ? is_active(c) && !is_attacking_or_has_attacked(c) :
-             mod == SkillMod::on_death ? (fd->tapi != src->m_player ? is_active(c) && !has_attacked(c) : is_active_next_turn(c)) :
+             mod == SkillMod::on_death ? c->m_index >= src->m_index && (fd->tapi != c->m_player ? is_active(c) : is_active_next_turn(c)) :
              is_active(c) || is_active_next_turn(c)));
 }
 
@@ -1646,7 +1646,7 @@ inline bool skill_predicate<jam>(Field* fd, CardStatus* src, CardStatus* c, cons
     const auto& mod = std::get<4>(s);
     return(can_act(c) &&  // (fd->tapi == c->m_player ? is_active(c) && !is_attacking_or_has_attacked(c) : is_active_next_turn(c)));
             (mod == SkillMod::on_attacked ? is_active(c) && !is_attacking_or_has_attacked(c) :
-             mod == SkillMod::on_death ? (fd->tapi != src->m_player ? is_active(c) && !has_attacked(c) : is_active_next_turn(c)) :
+             mod == SkillMod::on_death ? c->m_index >= src->m_index && (fd->tapi != c->m_player ? is_active(c) : is_active_next_turn(c)) :
              is_active(c) || is_active_next_turn(c)));
 }
 
@@ -1694,7 +1694,7 @@ inline bool skill_predicate<weaken>(Field* fd, CardStatus* src, CardStatus* c, c
     const auto& mod = std::get<4>(s);
     return(can_attack(c) && attack_power(c) > 0 &&  // (fd->tapi == c->m_player ? is_active(c) && !is_attacking_or_has_attacked(c) : is_active_next_turn(c)));
             (mod == SkillMod::on_attacked ? is_active(c) && !is_attacking_or_has_attacked(c) :
-             mod == SkillMod::on_death ? (fd->tapi != src->m_player ? is_active(c) && !has_attacked(c) : is_active_next_turn(c)) :
+             mod == SkillMod::on_death ? c->m_index >= src->m_index && (fd->tapi != c->m_player ? is_active(c) : is_active_next_turn(c)) :
              is_active(c) || is_active_next_turn(c)));
 }
 
